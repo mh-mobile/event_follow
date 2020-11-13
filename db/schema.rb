ed39174a00fd103462bfecafafe80ea95cc19e01
mh-mobile <mh.mobiler@gmail.com>
2020-11-14 02:26:38 +0900

    Eventモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_155655) do
+ActiveRecord::Schema.define(version: 2020_11_13_172235) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "site_id", null: false
+    t.integer "site_event_id", null: false
+    t.string "title", null: false
+    t.datetime "started_at", null: false
+    t.datetime "ended_at", null: false
+    t.string "banner", null: false
+    t.string "url", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["site_id", "site_event_id"], name: "index_events_on_site_and_site_event_id", unique: true
+    t.index ["site_id"], name: "index_events_on_site_id"
+  end
 
   create_table "sites", force: :cascade do |t|
     t.string "name"
@@ -37,5 +50,6 @@ ActiveRecord::Schema.define(version: 2020_11_13_155655) do
     t.string "profile_image", null: false
   end
 
+  add_foreign_key "events", "sites"
   add_foreign_key "user_tokens", "users"
 end
