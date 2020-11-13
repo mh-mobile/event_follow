18196dff6b6de08f0ff2eb6d2773b6aaa513f135
mh-mobile <mh.mobiler@gmail.com>
2020-11-13 23:45:46 +0900

    rubocopの設定を追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class EventsController < ApplicationController
   def index
   end
--- db/migrate/20201113082949_create_users.rb
+++ db/migrate/20201113082949_create_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[6.0]
   def up
     create_table :users, force: :cascade, id: false do |t|
