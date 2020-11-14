93e7a3f12d40c895b27b877d85a8cdac9b45678d
mh-mobile <mh.mobiler@gmail.com>
2020-11-14 18:03:44 +0900

    Friedshipモデルの関連を追加

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-class Friendship < ApplicationRecord
-  belongs_to :follower
-  belongs_to :followed
+class Friendship < ActiveRecord::Base
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
