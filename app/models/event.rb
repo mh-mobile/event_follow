641928aef58816685cd5466606da2973fc9122c8
mh-mobile <mh.mobiler@gmail.com>
2020-11-14 19:28:59 +0900

    Kaminariでイベント一覧を10件表示

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,21 +5,20 @@ class Event < ActiveRecord::Base
 
   belongs_to :site
   has_many :tweets, -> { order(tweeted_at: :desc) }, dependent: :delete_all
-  paginates_per 50
+  paginates_per 10
 
   scope :following_events, -> (user, event_sort_type, event_filter_type) {
-    scope = joins(:tweets)
-    .merge(Tweet.following_tweets(user))
-    .group("id")
-    .select("events.*")
-    if event_sort_type == "friends_number_order"
-      scope = scope.having(Event.time_filter_condition(event_filter_type))
-    else
-      scope = scope.having(Event.friends_filter_condition(event_filter_type))
-    end
-    scope.order(Arel.sql(Event.event_sort_condition(event_sort_type)))
-    .preload(tweets: :restricted_user)
-  }
+     scope = joins(:tweets)
+     .merge(Tweet.following_tweets(user))
+     .group("id")
+     .select("events.*")
+     if event_sort_type == "friends_number_order"
+       scope = scope.having(Event.time_filter_condition(event_filter_type))
+     else
+       scope = scope.having(Event.friends_filter_condition(event_filter_type))
+     end
+     scope.order(Arel.sql(Event.event_sort_condition(event_sort_type)))
+   }
 
   validates :site_id, presence: true
   validates :site_event_id, presence: true
