ecc988ee29aa88533fc9ba4e8aae4d01a2e12dbb
mh-mobile <mh.mobiler@gmail.com>
2020-11-14 16:51:55 +0900

    rubocopの適用

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Friendship < ApplicationRecord
   belongs_to :follower
   belongs_to :followed
--- db/migrate/20201114044126_create_user_event_settings.rb
+++ db/migrate/20201114044126_create_user_event_settings.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUserEventSettings < ActiveRecord::Migration[6.0]
   def change
     create_table :user_event_settings do |t|
