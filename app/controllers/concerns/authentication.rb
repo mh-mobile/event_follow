c731faa5d38babd0271a3cafb0c8ead7e9004483
mh-mobile <mh.mobiler@gmail.com>
2020-11-14 22:44:59 +0900

    ログアウト処理を追加

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -11,6 +11,11 @@ module Authentication
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
