e5f3699908402c04445f7abc8764cc85bd150cf2
mh-mobile <mh.mobiler@gmail.com>
2020-11-14 13:36:10 +0900

    Tweetモデルの作成

---
+++ db/migrate/20201114041618_create_tweets.rb
@@ -0,0 +1,22 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets, id: false do |t|
+      t.bigint :id, null: false
+      t.text :text, null: false
+      t.datetime :tweeted_at, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :quoted_tweet, null: true
+      t.references :retweeted_tweet, null: true
+      t.references :event, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    reversible do |direction|
+      direction.up { execute "ALTER TABLE tweets ADD PRIMARY KEY (id);" }
+    end
+
+    add_foreign_key :tweets, :tweets, column: :quoted_tweet_id
+    add_foreign_key :tweets, :tweets, column: :retweeted_tweet_id
+  end
+end
