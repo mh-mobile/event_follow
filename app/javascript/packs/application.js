31eadd0f7f2a2002d04715055f2b98f665321456
mh-mobile <mh.mobiler@gmail.com>
2020-11-17 18:37:40 +0900

    ソート＆絞り込みのVueコンポーネントを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 require('../profile_setting.js')
+require('../event_sort_filter.js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
