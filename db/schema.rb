0d54bac3e406d8e6cbf71dfaa034ab3bb204df84
mh-mobile <mh.mobiler@gmail.com>
2020-11-20 13:36:30 +0900

    イベントに紐づくフォロワーの関連ツイートを取得

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_222552) do
+ActiveRecord::Schema.define(version: 2020_11_20_012322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
