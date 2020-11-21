957fa3eafede6c5222da81e68376acb62790be2b
mh-mobile <mh.mobiler@gmail.com>
2020-11-21 20:33:53 +0900

    指定したユーザーIDをもとに友達一覧を返すように変更

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -2,7 +2,17 @@
 
 class Api::FriendshipsController < Api::BaseController
   def index
-    @users = current_user.following
+    user_ids = find_user_ids
+    @users = User.where(id: find_user_ids)
   end
+
+  private
+    def find_user_ids
+      user_ids = params[:user_ids]
+      if user_ids.present? && user_ids.split(",").count > 0
+        user_ids.split(",")
+      else
+        @user_ids = []
+      end
+    end
 end
-  
