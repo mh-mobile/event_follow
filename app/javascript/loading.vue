1284b18f960ea152707aaa045dc162a7d36a9a43
mh-mobile <mh.mobiler@gmail.com>
2020-11-21 18:11:40 +0900

    API通信中のローディングをツイート一覧に追加

---
+++ app/javascript/loading.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.loader-wrapper
+  .loader.is-loadingg
+</template>
+
+<style lang="scss" scoped>
+
+.loader-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .loader {
+    height: 50px;
+    width: 50px;
+  }
+
+  .is-loading {
+   position: relative;
+  }
+
+  &.is-active {
+    opacity: 1;
+    z-index: 1;
+  }
+}
+
+</style>
+
