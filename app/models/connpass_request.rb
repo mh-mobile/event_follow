8032fd1d3ad0d29dd59f4b229d0d09747e6e7d05
mh-mobile <mh.mobiler@gmail.com>
2020-11-22 23:10:15 +0900

    APIクライアントの初期化処理

--- app/models/connpass_request.rb
+++ app/models/connpass_request.rb
@@ -3,7 +3,9 @@
 class ConnpassRequest
   include EventRequestable
 
-  def initialize(api_client, ogp_client)
+  def initialize(api_client, ogp_client, event_url)
+    @api_client = api_client
+    @ogp_client = ogp_client
   end
 
   def request
--- app/models/tech_play_request.rb
+++ app/models/tech_play_request.rb
@@ -3,7 +3,9 @@
 class TechPlayRequest
   include EventRequestable
 
-  def initialize(api_client, ogp_client)
+  def initialize(ogp_client, event_url)
+    @ogp_client = ogp_client
+    @event_url = event_rul
   end
 
   def request
