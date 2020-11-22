6026754eb9d0f6942d42d511f6a149717b3ffc22
mh-mobile <mh.mobiler@gmail.com>
2020-11-23 03:26:01 +0900

    Doorkeeperのイベント情報の取得処理実装

--- app/models/doorkeeper_request.rb
+++ app/models/doorkeeper_request.rb
@@ -7,8 +7,21 @@ class DoorkeeperRequest
     @api_client = api_client
     @ogp_client = ogp_client
     @event_url = event_url
+    if match = event_url.match(/https:\/\/.*doorkeeper.jp\/events\/(?<event_id>\d+)/)
+      @event_id = match[:event_id]
+    end
   end
 
   def request
+    event = @api_client.search(@event_id).event
+    ogp_res = @ogp_client.search(@event_url)
+    Event.new(site_id: 2,
+        site_event_id: event.id,
+        title: event.title,
+        description: event.description,
+        started_at: event.starts_at,
+        ended_at: event.ends_at,
+        url: event.public_url,
+        banner: ogp_res.image.url)
   end
 end
