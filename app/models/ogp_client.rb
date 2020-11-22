bfd3025eba2090373e1b3180a174585200f2e7aa
mh-mobile <mh.mobiler@gmail.com>
2020-11-22 22:45:56 +0900

    OGPクライアントの実装

--- app/models/ogp_client.rb
+++ app/models/ogp_client.rb
@@ -5,5 +5,30 @@ class OgpClient
   end
 
   def search(event_url)
+    @endpoint = event_url
+    with_error_handling do
+      response = connection.get("")
+      open_graph = OGP::OpenGraph.new(response.body)
+    end
   end
+
+  private
+    def connection(endpoint: @endpoint)
+      Faraday.new(url: endpoint, headers: nil) do |connection|
+        connection.response :raise_error
+        connection.response :raise_error
+        connection.adapter Faraday.default_adapter
+      end
+    end
+
+    def with_error_handling
+      yield
+    rescue Faraday::ClientError, Faraday::ServerError => error
+      begin
+        body = JSON.parse(error.response[:body])
+        raise EventResponse, body["message"]
+      rescue JSON::ParserError
+        raise error
+      end
+    end
 end
