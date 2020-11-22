04e4a384da5543946c32ad028515abe1853da29d
mh-mobile <mh.mobiler@gmail.com>
2020-11-22 23:10:15 +0900

    APIクライアントの初期化処理

--- app/models/doorkeeper_request.rb
+++ app/models/doorkeeper_request.rb
@@ -3,7 +3,10 @@
 class DoorkeeperRequest
   include EventRequestable
 
-  def initialize(api_client, ogp_client)
+  def initialize(api_client, ogp_client, event_url)
+    @api_client = api_client
+    @ogp_client = ogp_client
+    @event_url = event_url
   end
 
   def request
