5312fd671f360edc1e8d205c4f482717a9d76146
mh-mobile <mh.mobiler@gmail.com>
2020-11-22 18:51:03 +0900

    イベント用のリクエストクラスの空メソッドを定義

--- app/models/connpass_request.rb
+++ app/models/connpass_request.rb
@@ -2,4 +2,10 @@
 
 class ConnpassRequest
   include EventRequestable
+
+  def initialize(api_client, ogp_client)
+  end
+
+  def request 
+  end
 end
--- app/models/doorkeeper_request.rb
+++ app/models/doorkeeper_request.rb
@@ -2,4 +2,10 @@
 
 class DoorkeeperRequest
   include EventRequestable
+
+  def initialize(api_client, ogp_client)
+  end
+
+  def request 
+  end
 end
