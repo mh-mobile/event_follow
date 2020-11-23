dde4e4fb6317d999c4c635e05a530213220698c5
mh-mobile <mh.mobiler@gmail.com>
2020-11-23 14:19:18 +0900

    イベントURLの正規化処理

--- app/models/concerns/event_requestable.rb
+++ app/models/concerns/event_requestable.rb
@@ -2,17 +2,17 @@
 
 module EventRequestable
   def self.create(event_url)
-    if event_url.include?("connpass.com/event/")
-      return ConnpassRequest.new(ConnpassClient.new, OgpClient.new, event_url)
+    if match = event_url.match(/(?<event_url>https:\/\/.*connpass.com\/event\/(?<event_id>\d+)\/)/)
+      return ConnpassRequest.new(ConnpassClient.new, OgpClient.new, match[:event_url], match[:event_id])
     end
 
-    if event_url.include?("doorkeeper.jp/events/")
+    if match = event_url.match(/(?<event_url>https:\/\/.*doorkeeper.jp\/events\/(?<event_id>\d+))/)
       auth_token = ENV["DOORKEEPER_AUTH_TOKEN"]
-      return DoorkeeperRequest.new(DoorkeeperClient.new(auth_token), OgpClient.new, event_url)
+      return DoorkeeperRequest.new(DoorkeeperClient.new(auth_token), OgpClient.new, match[:event_url], match[:event_id])
     end
 
-    if event_url.include?("techplay.jp/event/")
-      return TechPlayRequest.new(OgpClient.new, event_url)
+    if match = event_url.match(/(?<event_url>https:\/\/.*techplay.jp\/event\/(?<event_id>\d+))/)
+      return TechPlayRequest.new(OgpClient.new, match[:event_url], match[:event_id])
     end
 
     nil
