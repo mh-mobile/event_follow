e36f43a6e52ff8e35578dbbecd415dd1d0a27b81
mh-mobile <mh.mobiler@gmail.com>
2020-11-23 18:06:54 +0900

    イベントサイトのロゴを追加

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -8,4 +8,15 @@ module EventDecorator
     striped = striped[0, MAX_LENGTH] + "..." if striped.length > MAX_LENGTH
     striped
   end
+
+  def logo
+    case self.site_id
+    when Site::CONNPASS_EVENT_SITE_ID
+      image_tag "connpass_logo.png"
+    when Site::DOORKEEPER_EVENT_SITE_ID
+      image_tag "doorkeeper_logo.png"
+    when Site::TECHPLAY_EVENT_SITE_ID
+      image_tag ""
+    end
+  end
 end
