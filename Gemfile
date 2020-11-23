6a526f8d3675ebd8b1e35d57e82168746ad38bd1
mh-mobile <mh.mobiler@gmail.com>
2020-11-23 22:44:15 +0900

    Twitter APIの認証ヘッダーの作成と検索リクエスト処理の実装

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development do
   gem "listen", "~> 3.2"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+  gem "faraday_curl"
 end
 
 group :test do
--- app/models/twitter_request.rb
+++ app/models/twitter_request.rb
@@ -1,16 +1,28 @@
 # frozen_string_literal: true
 
 class TwitterRequest
+  def self.create(oauth_token:, oauth_token_secret:)
+    twitter_client = TwitterClient.new(
+      app_token: ENV["OAUTH_APP_TOKEN"],
+      oauth_consumer_key: ENV["OAUTH_CONSUMER_KEY"],
+      oauth_token: oauth_token,
+      oauth_consumer_secret: ENV["OAUTH_CONSUMER_SECRET"],
+      oauth_token_secret: oauth_token_secret
+    )
+    TwitterRequest.new(twitter_client)
+  end
+
   def initialize(twitter_client)
     @twitter_client = twitter_client
   end
 
-  def tweets(since_id)
+  def tweets(since_id: 0)
+    @twitter_client.search(q: "(url:doorkeeper.jp/events OR url:connpass.com/event/) filter:links", since_id: since_id)
   end
 
   def quoted_tweets
   end
 
-  def following(user_id, since_id)
+  def following(user_id:, since_id:)
   end
 end
