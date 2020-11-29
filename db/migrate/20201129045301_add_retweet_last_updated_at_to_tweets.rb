d5f13dafa5d6bcd9f71b049b8472a6ddb27ccf8a
mh-mobile <mh.mobiler@gmail.com>
2020-11-29 14:21:22 +0900

    引用RTの最終更新日時のカラムを追加するテーブルの誤りを修正

--- db/migrate/20201129045301_add_retweet_last_updated_at_to_tweets.rb
+++ db/migrate/20201129045301_add_retweet_last_updated_at_to_tweets.rb
@@ -2,6 +2,6 @@
 
 class AddRetweetLastUpdatedAtToTweets < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :retweet_last_updated_at, :datetime, null: true
+    add_column :tweets, :retweet_last_updated_at, :datetime, null: true
   end
 end
