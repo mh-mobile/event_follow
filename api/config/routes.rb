84ecfa75e635f0114cefbee0aeb24d263b2eb6e4
mh-mobile <mh.mobiler@gmail.com>
2020-12-12 00:06:07 +0900

    APIモードのroutes.rbを追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace "api" do
+    resources :following_tweets, only: %i(index)
+    resources :friendships, only: %i(index)
+  end
 end
