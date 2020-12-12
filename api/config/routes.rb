fef0fef3b3ceb6f40c299eb7566d8f641c88047d
mh-mobile <mh.mobiler@gmail.com>
2020-12-12 12:26:50 +0900

    frinedships apiでデフォルトでjsonを返すように修正

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace "api" do
+  namespace "api", defaults: { format: "json" } do
     resources :following_tweets, only: %i(index)
     resources :friendships, only: %i(index)
   end
