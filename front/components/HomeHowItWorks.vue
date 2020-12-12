f8c80265683204521eed9fb134be071cfc82fd16
mh-mobile <mh.mobiler@gmail.com>
2020-12-12 19:05:41 +0900

    Nuxt.jsのページコンポーネントでホーム画面実装

---
+++ front/components/HomeHowItWorks.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+.home_how_it_works
+  .how_it_works_label
+    | How It Works
+  .how_it_works_content
+    .how_it_works_description
+      | Twitterでフォローしている友達がシェアしたツイートに基づいて、イベント情報を発見します
+    .how_it_works_picture
+      img(src="@/assets/how_it_works_picture.png")
+</template>
+
+<style lang="scss" scoped>
+.home_how_it_works {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+
+  .how_it_works_label {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+
+  .how_it_works_content {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-center;
+
+    .how_it_works_description {
+      width: 50%;
+      font-size: 1.2em;
+      padding: 10px;
+      display: flex;
+      align-items: center;
+    }
+
+    .how_it_works_picture {
+      width: 50%;
+      height: auto;
+      padding: 10px;
+    }
+  }
+}
+</style>
---
+++ front/components/HomeTwitterLogin.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+.home_twitter_login
+  a(href="/auth/twitter" class="twitter_login_button") Twitterで始める
+</template>
+
+<style lang="scss" scoped>
+.home_twitter_login {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .twitter_login_button {
+    background: #218af5;
+    color: #fff;
+    font-size: 1em;
+    font-weight: bold;
+    padding: 15px 40px;
+    border-radius: 10px;
+    cursor: pointer;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      width: 40px;
+      height: 40px;
+      background-image: url("~@/assets/twitter_logo.png");
+      background-size: contain;
+      vertical-align: middle;
+    }
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+}
+</style>
