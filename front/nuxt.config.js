40557c6bd184fd6aaa74d917a41add61e2008bba
mh-mobile <mh.mobiler@gmail.com>
2020-12-12 16:48:59 +0900

    nuxtjs/composition-apiのモジュールをインストール

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -26,7 +26,8 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build'
+    '@nuxt/typescript-build',
+    '@nuxtjs/composition-api'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
