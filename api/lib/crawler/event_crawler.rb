ddbe32cd690e3a708ebc5186fe5f4d5df9182386
mh-mobile <mh.mobiler@gmail.com>
2021-01-02 18:11:06 +0900

    イベントクローラのクラス内部のループ処理を削除

--- api/lib/crawler/event_crawler.rb
+++ api/lib/crawler/event_crawler.rb
@@ -1,40 +1,32 @@
 # frozen_string_literal: true
 
 class EventCrawler
-  def start
-    loop do
-      crawl_tweets = CrawlTweet.order(tweeted_at: :asc).limit(1)
-      next if crawl_tweets.length == 0
+  def self.start
+    crawl_tweets = CrawlTweet.order(tweeted_at: :asc).limit(1)
+    return if crawl_tweets.length == 0
 
-      tweet = crawl_tweets.first
-      tweet_id = tweet.id
-      tweet_text = tweet.text
-      tweeted_at = tweet.tweeted_at
-      event_url = tweet.event_url
-      user_id = tweet.user_id
+    tweet = crawl_tweets.first
+    tweet_id = tweet.id
+    tweet_text = tweet.text
+    tweeted_at = tweet.tweeted_at
+    event_url = tweet.event_url
+    user_id = tweet.user_id
 
-      params = { event_url: event_url }
-      event_store = EventStore.new(params)
-      if event_store.save
-        event_id = event_store.event.id
-        tweet = Tweet.new(
-          id: tweet_id,
-          text: tweet_text,
-          tweeted_at: tweeted_at,
-          event_id: event_id,
-          user_id: user_id,
-          quoted_tweet_id: nil,
-          retweeted_tweet_id: nil
-        )
-        tweet.save
-      end
-      CrawlTweet.find(tweet_id).destroy
-    ensure
-      sleep 5
+    params = { event_url: event_url }
+    event_store = EventStore.new(params)
+    if event_store.save
+      event_id = event_store.event.id
+      tweet = Tweet.new(
+        id: tweet_id,
+        text: tweet_text,
+        tweeted_at: tweeted_at,
+        event_id: event_id,
+        user_id: user_id,
+        quoted_tweet_id: nil,
+        retweeted_tweet_id: nil
+      )
+      tweet.save
     end
-  end
-
-  def stop
-    puts "stop  : #{Time.now}"
+    CrawlTweet.find(tweet_id).destroy
   end
 end
