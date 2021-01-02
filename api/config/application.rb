c35ec7ffb360ac758ae2b826e8e3f0cbbe10b78e
mh-mobile <mh.mobiler@gmail.com>
2021-01-02 15:21:13 +0900

    クローラ用のautoload_pathの設定を追加

--- api/config/application.rb
+++ api/config/application.rb
@@ -38,5 +38,6 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths += Dir["#{config.root}/lib/crawler"]
   end
 end
