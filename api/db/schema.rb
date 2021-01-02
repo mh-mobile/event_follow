6b0cfa988cbfc84b6619bd1769dce6198c68733e
mh-mobile <mh.mobiler@gmail.com>
2021-01-02 15:18:24 +0900

    ツイートクロール用の設定値保存のテーブル定義を作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_045301) do
+ActiveRecord::Schema.define(version: 2021_01_02_051811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 2020_11_29_045301) do
 
   create_table "sites", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tweet_crawl_settings", force: :cascade do |t|
+    t.string "max_id", default: "0", null: false
+    t.string "since_id", default: "0", null: false
+    t.string "search_base_max_id", default: "0", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
