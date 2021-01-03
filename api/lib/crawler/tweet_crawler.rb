98d6fce68e61f6cda601980cf590e45b6442a869
mh-mobile <mh.mobiler@gmail.com>
2021-01-03 15:04:50 +0900

    ツイートのクロール設定値処理の共通化

--- api/lib/crawler/tweet_crawler.rb
+++ api/lib/crawler/tweet_crawler.rb
@@ -5,25 +5,21 @@
   include TweetCrawlable
 
    def self.start
-     crawl_setting = TweetCrawlSetting.first
-     @max_id = crawl_setting.max_id
-     @since_id = crawl_setting.since_id
-     @search_base_max_id = crawl_setting.search_base_max_id
 
      twitter_request = TwitterRequest.create
-     tweets = twitter_request.tweets(max_id: @max_id, since_id: @since_id)
+     tweets = twitter_request.tweets(max_id: crawl_setting_value[:max_id], since_id: crawl_setting_value[:since_id])
 
      return if tweets.statuses.count == 0
 
      if tweets.statuses.count == TwitterClient::SEARCH_COUNT
-       @search_base_max_id = (tweets.statuses.first.id_str) if @max_id == "0"
-       @max_id = (tweets.statuses.last.id_str.to_i - 1).to_s
+       crawl_setting_value[:search_base_max_id] = (tweets.statuses.first.id_str) if crawl_setting_value[:max_id] == "0"
+       crawl_setting_value[:max_id] = (tweets.statuses.last.id_str.to_i - 1).to_s
      else
-       @max_id = "0"
-       @since_id = @search_base_max_id
+       crawl_setting_value[:max_id] = "0"
+       crawl_setting_value[:since_id] = crawl_setting_value[:search_base_max_id]
      end
 
-     crawl_setting.update(max_id: @max_id, since_id: @since_id, search_base_max_id: @search_base_max_id)
+     update_crawl_setting
 
      User.insert_all(tweet_users(tweets.statuses.map(&:user)))
      CrawlTweet.insert_all(inserted_crawl_tweets(tweets.statuses))
