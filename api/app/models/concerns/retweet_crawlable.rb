ac20a5999a722fb2668a99633e9a44354a09f758
mh-mobile <mh.mobiler@gmail.com>
2021-01-03 19:36:11 +0900

    リツイートクローラのリファクタリング

--- api/app/models/concerns/retweet_crawlable.rb
+++ api/app/models/concerns/retweet_crawlable.rb
@@ -1,35 +1,68 @@
+# frozen_string_literal: true
+
 module RetweetCrawlable
   extend ActiveSupport::Concern
 
-  module ClassMethods
-    def inserted_retweet_objects(tweets, event_id: ,retweeted_tweet_id: nil, quoted_tweet_id: nil)
-      time = Time.current
-      tweets.statuses.map do |tweet|
-        {
-          id: tweet.id_str,
-          text: tweet.text,
-          tweeted_at:  DateTime.parse(tweet.created_at).utc.iso8601,
-          user_id: tweet.user.id_str,
-          retweeted_tweet_id: retweeted_tweet_id,
-          quoted_tweet_id: quoted_tweet_id,
-          event_id: event_id,
-          created_at: time,
-          updated_at: time
-        }
-      end
-
-      def inserted_retweets(tweets, event_id: ,retweeted_tweet_id:)
-        inserted_retweet_objects(tweets, event_id: event_id,
-                                  retweeted_tweet_id: retweeted_tweet_id,
-                                  quoted_tweet_id: nil)
-      end
-
-      def inserted_quoted_retweets(tweets, event_id: ,quoted_tweet_id:)
-        inserted_retweet_objects(tweets, event_id: event_id,
-                                  retweeted_tweet_id: nil,
-                                  quoted_tweet_id: quoted_tweet_id)
-      end
+  def target_tweet
+    @target_tweet ||=begin
+      Tweet.normal_tweets
+      .where(tweeted_at: Time.current.ago(24.hours)..Time.current.ago(4.hours))
+      .order(Arel.sql("retweet_last_updated_at asc NULLS FIRST")).limit(1).first
     end
   end
 
+  def twitter_request
+    @twitter_request ||= TwitterRequest.create
+  end
+
+  def search_retweets(target_tweet)
+    @retweets ||= twitter_request.retweets(target_tweet.id)
+  end
+
+  def search_quoted_retweets(target_tweet)
+    tweet_url = "https://twitter.com/#{target_tweet.user.screen_name}/status/#{target_tweet.id}"
+    @quoted_retweets ||= twitter_request.quoted_tweets(tweet_url).statuses
+  end
+
+  def update_target_tweet(target_tweet)
+    time = Time.current
+    target_tweet.update(retweet_last_updated_at: time)
+  end
+
+  def update_tweets_users(retweets)
+    User.insert_all(tweet_users(retweets.map(&:user)))
+  end
+
+  def update_retweets(retweets)
+    Tweet.insert_all(retweets)
+  end
+
+  def inserted_retweet_objects(tweets, event_id:, retweeted_tweet_id: nil, quoted_tweet_id: nil)
+    time = Time.current
+    tweets.map do |tweet|
+      {
+        id: tweet.id_str,
+        text: tweet.text,
+        tweeted_at:  DateTime.parse(tweet.created_at).utc.iso8601,
+        user_id: tweet.user.id_str,
+        retweeted_tweet_id: retweeted_tweet_id,
+        quoted_tweet_id: quoted_tweet_id,
+        event_id: event_id,
+        created_at: time,
+        updated_at: time
+      }
+    end
+
+    def inserted_retweets(tweets, event_id:, retweeted_tweet_id:)
+      inserted_retweet_objects(tweets, event_id: event_id,
+                                retweeted_tweet_id: retweeted_tweet_id,
+                                quoted_tweet_id: nil)
+    end
+
+    def inserted_quoted_retweets(tweets, event_id:, quoted_tweet_id:)
+      inserted_retweet_objects(tweets, event_id: event_id,
+                                retweeted_tweet_id: nil,
+                                quoted_tweet_id: quoted_tweet_id)
+    end
+  end
 end
--- api/lib/crawler/retweet_crawler.rb
+++ api/lib/crawler/retweet_crawler.rb
@@ -2,31 +2,28 @@
 
 class RetweetCrawler
   include BaseCrawlable
+  include RetweetCrawlable
 
-  def self.start
-    target_tweet = Tweet.normal_tweets
-                          .where(tweeted_at: Time.current.ago(24.hours)..Time.current.ago(4.hours))
-                        .or(Tweet.normal_tweets
-                          .where(retweet_last_updated_at: nil)
-                          .where(tweeted_at: Time.current.ago(24.hours)..Time.current.ago(4.hours)))
-                        .order(Arel.sql("retweet_last_updated_at asc NULLS FIRST")).limit(1).first
+  def start
     return unless target_tweet
+    update_target_tweet(target_tweet)
+    process_retweets(target_tweet)
+    process_quoted_retweets(target_tweet)
+  end
 
-    twitter_request = TwitterRequest.create
-    retweets = twitter_request.retweets(target_tweet.id)
+  def process_retweets(target_tweet)
+    retweets = search_retweets(target_tweet)
+    return if retweets.empty?
+    update_tweets_users(retweets)
+    update_retweets(inserted_retweets(retweets, event_id: target_tweet.event_id,
+                                                retweeted_tweet_id: target_tweet.id))
+  end
 
-    time = Time.current
-    target_tweet.update(retweet_last_updated_at: time)
-
-    User.insert_all(tweet_users(retweets.map(&:user)))
-    Tweet.insert_all(inserted_retweets(retweets, event_id: target_tweet.event_id, retweeted_tweet_id: target_tweet.id))
-
-    tweet_url = "https://twitter.com/#{target_tweet.user.screen_name}/status/#{target_tweet.id}"
-    quoted_retweets = twitter_request.quoted_tweets(tweet_url)
-
-    return if quoted_retweets.statuses.count == 0
-
-    User.insert_all(tweet_users(quoted_retweets.statuses.map(&:user)))
-    Tweet.insert_all(inserted_quoted_retweets(quoted_retweets, event_id: target_tweet.event_id, quoted_tweet_id: target_tweet.id))
+  def process_quoted_retweets(target_tweet)
+    quoted_retweets = search_quoted_retweets(target_tweet)
+    return if quoted_retweets.empty?
+    update_tweets_users(quoted_retweets)
+    update_retweets(inserted_quoted_retweets(quoted_retweets, event_id: target_tweet.event_id,
+                                                              quoted_tweet_id: target_tweet.id))
   end
 end
