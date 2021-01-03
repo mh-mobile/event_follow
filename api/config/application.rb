daf3deb4e4ee577e837da93a1b5fd913d945661f
mh-mobile <mh.mobiler@gmail.com>
2021-01-03 20:46:51 +0900

    ActiveStorage関連を削除

--- api/config/application.rb
+++ api/config/application.rb
@@ -6,7 +6,6 @@ require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
 require "action_controller/railtie"
 require "action_text/engine"
 require "action_view/railtie"
