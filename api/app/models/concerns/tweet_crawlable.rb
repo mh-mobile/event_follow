b6be8fd46b2bbc5fbd9b82f354361f12d9253d4a
mh-mobile <mh.mobiler@gmail.com>
2021-01-03 15:51:18 +0900

    ツイートクローラのリファクタリング

--- api/app/models/concerns/tweet_crawlable.rb
+++ api/app/models/concerns/tweet_crawlable.rb
@@ -1,40 +1,67 @@
+# frozen_string_literal: true
+
 module TweetCrawlable
   extend ActiveSupport::Concern
 
-  module ClassMethods
-    def inserted_crawl_tweets(tweets)
-      time = Time.current
-      tweets.map do |tweet|
-        {
-          id: tweet.id_str,
-          text: tweet.text,
-          tweeted_at:  DateTime.parse(tweet.created_at).utc.iso8601,
-          user_id: tweet.user.id_str,
-          event_url: tweet.entities.urls.first&.expanded_url || "",
-          created_at: time,
-          updated_at: time
-        }
-      end
-    end
-
-    def crawl_setting_value
-      @crawl_setting_value ||= tweet_crawl_setting
-    end
-
-    def tweet_crawl_setting
-      setting = TweetCrawlSetting.first
+  def inserted_crawl_tweets(tweets)
+    time = Time.current
+    tweets.map do |tweet|
       {
-        max_id: setting.max_id,
-        since_id: setting.since_id,
-        search_base_max_id: setting.search_base_max_id
+        id: tweet.id_str,
+        text: tweet.text,
+        tweeted_at:  DateTime.parse(tweet.created_at).utc.iso8601,
+        user_id: tweet.user.id_str,
+        event_url: tweet.entities.urls.first&.expanded_url || "",
+        created_at: time,
+        updated_at: time
       }
     end
+  end
 
-    def update_crawl_setting
-      TweetCrawlSetting.first.update(max_id: crawl_setting_value[:max_id],
-                                     since_id: crawl_setting_value[:since_id],
-                                     search_base_max_id: crawl_setting_value[:search_base_max_id])
+  def crawl_setting_value
+    @crawl_setting_value ||= tweet_crawl_setting
+  end
+
+  def tweet_crawl_setting
+    setting = TweetCrawlSetting.first
+    {
+      max_id: setting.max_id,
+      since_id: setting.since_id,
+      search_base_max_id: setting.search_base_max_id
+    }
+  end
+
+  def search_tweets
+    @tweets ||= begin
+      twitter_request = TwitterRequest.create
+      twitter_request.tweets(max_id: crawl_setting_value[:max_id], since_id: crawl_setting_value[:since_id])
+    end
+    @tweets.statuses
+  end
+
+  def tweets_max_fetched?(tweets)
+    tweets.count == TwitterClient::SEARCH_COUNT
+  end
+
+  def update_crawl_setting(tweets)
+    if tweets_max_fetched?(tweets)
+      crawl_setting_value[:search_base_max_id] = (tweets.first.id_str) if crawl_setting_value[:max_id] == "0"
+      crawl_setting_value[:max_id] = (tweets.last.id_str.to_i - 1).to_s
+    else
+      crawl_setting_value[:max_id] = "0"
+      crawl_setting_value[:since_id] = crawl_setting_value[:search_base_max_id]
     end
 
+    TweetCrawlSetting.first.update(max_id: crawl_setting_value[:max_id],
+                                   since_id: crawl_setting_value[:since_id],
+                                   search_base_max_id: crawl_setting_value[:search_base_max_id])
+  end
+
+  def update_tweet_users(tweets)
+    User.insert_all(tweet_users(tweets.map(&:user)))
+  end
+
+  def update_crawl_tweets(tweets)
+    CrawlTweet.insert_all(inserted_crawl_tweets(tweets))
   end
 end
