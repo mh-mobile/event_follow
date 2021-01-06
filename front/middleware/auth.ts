44d78733aa1cb6e4252495b4ec66044d787dc38d
mh-mobile <mh.mobiler@gmail.com>
2021-01-07 06:23:12 +0900

    未ログイン時のhome以外のURLアクセスの制御

--- front/middleware/auth.ts
+++ front/middleware/auth.ts
@@ -2,11 +2,7 @@ import { defineNuxtMiddleware } from "@nuxtjs/composition-api"
 import "firebase/auth"
 
 export default defineNuxtMiddleware(({ store, route, redirect }) => {
-  if (!store.state.auth && route.name === "events") {
-    return redirect("/home")
-  }
-
-  if (!store.state.auth && route.name === "index") {
+  if (!store.state.auth && route.name !== "home") {
     return redirect("/home")
   }
 
