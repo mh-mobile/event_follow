abe7aa2bcad0f3e628f3a5b8492f322758623a6c
mh-mobile <mh.mobiler@gmail.com>
2021-01-11 15:29:39 +0900

    ツイートの収集間隔を5秒から30秒に変更

--- api/config/clock.rb
+++ api/config/clock.rb
@@ -20,7 +20,7 @@ module Clockwork
     end
   end
 
-  every(5.seconds, "tweet_crawler.job", thread: true)
+  every(30.seconds, "tweet_crawler.job", thread: true)
   every(10.seconds, "event_crawler.job", thread: true)
   every(5.seconds, "retweet_crawler.job", thread: true)
   every(1.minutes, "following_crawler.job", thread: true)
