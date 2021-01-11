b0b8a22b0f2939ee2b0731bb6f5031cf39b16e59
mh-mobile <mh.mobiler@gmail.com>
2021-01-11 15:28:29 +0900

    rubocopの適用

--- api/db/migrate/20210111042432_change_following_next_cursor_column_type_for_users.rb
+++ api/db/migrate/20210111042432_change_following_next_cursor_column_type_for_users.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class ChangeFollowingNextCursorColumnTypeForUsers < ActiveRecord::Migration[6.1]
   def up
     change_column :users, :following_next_cursor, :string, default: "-1"
   end
-  
+
   def down
-   raise ActiveRecord::IrreversibleMigration, "カラムの型をintegerに変更できません"
+    raise ActiveRecord::IrreversibleMigration, "カラムの型をintegerに変更できません"
   end
 end
