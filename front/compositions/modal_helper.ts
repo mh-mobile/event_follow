4d20760c3de4e1a1641721dacf7a3a1e1ddcb8e3
mh-mobile <mh.mobiler@gmail.com>
2021-01-14 00:39:05 +0900

    prettierの適用

--- front/compositions/modal_helper.ts
+++ front/compositions/modal_helper.ts
@@ -1,8 +1,7 @@
 export const useModalHelper = () => {
-
   const setScrollEnabled = (isEnabled: boolean) => {
     const htmlElement = document.querySelector("html")
-    if(isEnabled) {
+    if (isEnabled) {
       htmlElement?.classList.remove("scroll_disable")
     } else {
       htmlElement?.classList.add("scroll_disable")
