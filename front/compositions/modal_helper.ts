822519da34aa2a183c43d795a1efe5df34d6ee89
mh-mobile <mh.mobiler@gmail.com>
2021-01-14 00:35:22 +0900

    モーダル表示時にイベント一覧をスクロールしないように制御

---
+++ front/compositions/modal_helper.ts
@@ -0,0 +1,15 @@
+export const useModalHelper = () => {
+
+  const setScrollEnabled = (isEnabled: boolean) => {
+    const htmlElement = document.querySelector("html")
+    if(isEnabled) {
+      htmlElement?.classList.remove("scroll_disable")
+    } else {
+      htmlElement?.classList.add("scroll_disable")
+    }
+  }
+
+  return {
+    setScrollEnabled
+  }
+}
