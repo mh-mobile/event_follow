ce06967d33494f3d6dceb487058b7a6dd248953a
mh-mobile <mh.mobiler@gmail.com>
2021-01-17 22:32:48 +0900

    DockerのRedisの設定を追加

--- api/config/initializers/firebase_id_token.rb
+++ api/config/initializers/firebase_id_token.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new
+  config.redis = Redis.new(host: "redis")
   config.project_ids = ["firebase_project_id"]
 end
