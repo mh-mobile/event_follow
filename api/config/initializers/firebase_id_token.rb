3e6df4e3c3b4db8fb138f1dc0e40486fc7fd2298
mh-mobile <mh.mobiler@gmail.com>
2021-01-19 15:28:42 +0900

    FirebaseのRedisの設定を環境別に設定

--- api/config/initializers/firebase_id_token.rb
+++ api/config/initializers/firebase_id_token.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(host: "redis")
+  config.redis = Proc.new do
+    if Rails.env.production?
+      Redis.new(host: ENV["REDIS_URL"])
+    else
+      Redis.new(host: "redis")
+    end
+  end.call
+  
   config.project_ids = [ENV["FIREBASE_PROJECT_ID"]]
 end
