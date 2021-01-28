97594b9e31dc99811fe3661ee6987e6ab62835f8
mh-mobile <mh.mobiler@gmail.com>
2021-01-29 04:58:38 +0900

    設定画面のルーティングの認証制御を追加

--- front/middleware/auth.ts
+++ front/middleware/auth.ts
@@ -1,12 +1,19 @@
 import { defineNuxtMiddleware } from "@nuxtjs/composition-api"
 import "firebase/auth"
+import { state } from "~/store"
 
 export default defineNuxtMiddleware(({ store, route, redirect }) => {
   if (!store.state.auth && route.name !== "home") {
     return redirect("/home")
   }
 
-  if (store.state.auth && route.name !== "events") {
-    return redirect("/events")
+  if (!store.state.auth) {
+    return redirect("/home")
   }
+
+  if (route.name === "settings" || route.name === "events") {
+    return
+  }
+
+  return redirect("/events")
 })
