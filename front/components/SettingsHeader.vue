3ac21a7d7c354fca8def0f726320e560306418d0
mh-mobile <mh.mobiler@gmail.com>
2021-01-29 09:42:58 +0900

    設定画面に退会ボタンを追加

---
+++ front/components/SettingsHeader.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.event_list_header
+  .event_header_top
+    .event_header_info
+      | 設定
+</template>
+
+<style lang="scss" scoped>
+.event_list_header {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  .event_header_top {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+
+    .event_header_info {
+      margin-right: auto;
+      font-size: 2em;
+      font-weight: bold;
+    }
+  }
+}
+</style>
