79966ffd900f772a36a4cb5bca5c5459d73c18e8
mh-mobile <mh.mobiler@gmail.com>
2021-01-29 19:23:59 +0900

    利用規約の本文を作成

---
+++ front/pages/terms.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.terms_container
+  TermsContent
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api"
+import TermsContent from "@/components/TermsContent.vue"
+export default defineComponent({
+  layout: "unauthorized_default",
+  components: {
+    TermsContent
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.terms_container {
+  max-width: 50rem;
+  min-width: 50rem;
+  margin: 20px auto;
+  padding: 20px 30px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  min-height: calc(100vh - 200px);
+}
+</style>
