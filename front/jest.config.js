e3bcfc1bf9ba38d359aa9a82f5e3cce0ca1e98f6
mh-mobile <mh.mobiler@gmail.com>
2021-01-30 19:53:46 +0900

    jest.config.js に@nuxtjs/composition-apiの設定を追加

--- front/jest.config.js
+++ front/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "^~/(.*)$": "<rootDir>/$1",
-    "^vue$": "vue/dist/vue.common.js"
+    "^vue$": "vue/dist/vue.common.js",
+    "@nuxtjs/composition-api': '@nuxtjs/composition-api/lib/entrypoint.js"
   },
   moduleFileExtensions: ["ts", "js", "vue", "json"],
   transform: {
