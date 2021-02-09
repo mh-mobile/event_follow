92041939c495c436c02f1b8ece94e10bd7e56d38
mh-mobile <mh.mobiler@gmail.com>
2021-02-09 19:28:53 +0900

    Layout/TrailingWhitespaceのrubocopの警告対応

--- api/config/application.rb
+++ api/config/application.rb
@@ -39,7 +39,7 @@ module Api
     Sentry.init do |config|
       config.dsn = ENV["API_SENTRY_DSN"]
       config.breadcrumbs_logger = [:active_support_logger]
-    
+
       # To activate performance monitoring, set one of these options.
       # We recommend adjusting the value in production:
       config.traces_sample_rate = 0.5
