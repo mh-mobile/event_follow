6655a819b2ee28327b9e70753abdcb3398b52514
mh-mobile <mh.mobiler@gmail.com>
2021-02-14 19:41:27 +0900

    HomeConcept.vueのStoryを追加

---
+++ front/components/HomeConcept.stories.ts
@@ -0,0 +1,17 @@
+import HomeConcept from './HomeConcept'
+
+export default {
+  title: 'HomeConcept',
+  component: HomeConcept,
+  parameters: {
+    layout: "centered"
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { HomeConcept },
+  template: `<HomeConcept v-bind="$props" /> `
+})
+
+export const Default = Template.bind({})
