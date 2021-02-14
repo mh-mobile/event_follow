3db1c225a6a8657b306753e843062a108a544e29
mh-mobile <mh.mobiler@gmail.com>
2021-02-14 20:07:15 +0900

    利用規約とプライバシーポリシーのstoryを追加

---
+++ front/components/PrivacyPolicyContent.stories.ts
@@ -0,0 +1,17 @@
+import PrivacyPolicyContent from './PrivacyPolicyContent'
+
+export default {
+  title: 'PrivacyPolicyContent',
+  component: PrivacyPolicyContent,
+  parameters: {
+    layout: "centered"
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { PrivacyPolicyContent },
+  template: `<PrivacyPolicyContent v-bind="$props" /> `
+})
+
+export const Default = Template.bind({})
---
+++ front/components/TermsContent.stories.ts
@@ -0,0 +1,17 @@
+import TermsContent from './TermsContent'
+
+export default {
+  title: 'TermsContent',
+  component: TermsContent,
+  parameters: {
+    layout: "centered"
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { TermsContent },
+  template: `<TermsContent v-bind="$props" /> `
+})
+
+export const Default = Template.bind({})
