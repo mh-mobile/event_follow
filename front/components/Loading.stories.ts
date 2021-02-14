10c151f92af53fb94063b75a4d3a0d8d843995e8
mh-mobile <mh.mobiler@gmail.com>
2021-02-14 23:59:44 +0900

    Loading.vueのstoryを追加

---
+++ front/components/Loading.stories.ts
@@ -0,0 +1,17 @@
+import Loading from './Loading'
+
+export default {
+  title: 'Loading',
+  component: Loading,
+  parameters: {
+    layout: "centered"
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Loading },
+  template: `<Loading v-bind="$props" /> `
+})
+
+export const Default = Template.bind({})
