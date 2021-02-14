faa744ebd43dc73b050f3e3aa9c5c55b374ef671
mh-mobile <mh.mobiler@gmail.com>
2021-02-14 15:56:17 +0900

    .storybook/main.js にprettierを適用

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -1,25 +1,25 @@
-const path = require('path')
+const path = require("path")
 
 module.exports = {
-  stories: ['../components/**/*.stories.js'],
+  stories: ["../components/**/*.stories.js"],
   addons: [
-      {
-        name: '@storybook/addon-essentials',
-        options: {
-            actions: true,
-            backgrounds: true,
-            controls: true,
-            docs: true,
-            viewport: true,
-            toolbars: true
-        }
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        actions: true,
+        backgrounds: true,
+        controls: true,
+        docs: true,
+        viewport: true,
+        toolbars: true
       }
-    ],
-  webpackFinal: config => {
+    }
+  ],
+  webpackFinal: (config) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../')
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../")
     })
     return config
   }
