5c9f1aa0a766089a0b95edd171e0f8f50dfdf5f4
mh-mobile <mh.mobiler@gmail.com>
2021-02-15 23:56:37 +0900

    .storybookを削除

--- front/.storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
-import "bulma/css/bulma.css"
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  viewport: {
-    viewports: INITIAL_VIEWPORTS
-  }
-}
