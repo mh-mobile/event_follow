e12233f8a38daddfaa6628a6e80e9030444fa0e8
mh-mobile <mh.mobiler@gmail.com>
2021-02-16 16:23:27 +0900

    FriendsListのstoryを追加

---
+++ front/components/FriendsList.stories.js
@@ -0,0 +1,22 @@
+import FriendsList from './FriendsList'
+
+export default {
+  title: 'FriendsList',
+  component: FriendsList,
+  parameters: {
+    layout: "centered"
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { FriendsList },
+  template: `<FriendsList v-bind="$props" /> `
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  eventId: 1,
+  userIds: "111,222,3333",
+  friendsNumber: 3
+}
