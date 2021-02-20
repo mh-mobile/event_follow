d9a3493115e90589e3cf30911a81a38adb09a8ca
mh-mobile <mh.mobiler@gmail.com>
2021-02-20 20:06:05 +0900

    RSpecのAPIの認証部分をヘルパーで共通化

--- api/spec/requests/friendships_spec.rb
+++ api/spec/requests/friendships_spec.rb
@@ -6,21 +6,7 @@ RSpec.describe "Friendships", type: :request do
   describe "GET /friendships" do
     before do
       @hoge_user = create(:hoge)
-      jwt = JSON.parse(File.read(file_fixture("jwt.json")))
-      @token = jwt["jwt_token"]
-      @certificate = JSON.parse(File.read(file_fixture("certificates.json")))
-      @project_ids = ["firebase-id-token"]
-    end
-
-    def authenticate(token: @token)
-      @headers ||= {}
-      @headers["Authorization"] = "Bearer #{token}"
-      @headers
-    end
-
-    def stub_firebase_id_token
-      allow_any_instance_of(FirebaseIdToken::Certificates).to receive(:local_certs).and_return(@certificate)
-      allow_any_instance_of(FirebaseIdToken::Configuration).to receive(:project_ids).and_return(@project_ids)
+      setup_jwt_authentication
     end
 
     it "request without authorization" do
