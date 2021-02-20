168dbb561f68d7178906e67874a61284ade8c74d
mh-mobile <mh.mobiler@gmail.com>
2021-02-20 16:51:58 +0900

    Factoryファイルを追加

---
+++ api/spec/factories/friendships.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :default_friendship, class: Friendship do
+  end
+end
---
+++ api/spec/factories/user_event_settings.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :default_setting, class: UserEventSetting do
+  end
+end
