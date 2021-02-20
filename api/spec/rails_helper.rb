318f5c70db1da203b65f01aa00f852c0c3c3917d
mh-mobile <mh.mobiler@gmail.com>
2021-02-20 20:06:05 +0900

    RSpecのAPIの認証部分をヘルパーで共通化

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -22,7 +22,7 @@ require "rspec/rails"
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -72,4 +72,5 @@ RSpec.configure do |config|
   include Committee::Rails::Test::Methods
 
   config.include FactoryBot::Syntax::Methods
+  config.include AuthenticationHelper
 end
--- api/spec/requests/sessions_spec.rb
+++ api/spec/requests/sessions_spec.rb
@@ -8,10 +8,7 @@ end
 RSpec.describe "Sessions", type: :request do
   describe "POST /api/sessions" do
     before do
-      jwt = JSON.parse(File.read(file_fixture("jwt.json")))
-      @token = jwt["jwt_token"]
-      @certificate = JSON.parse(File.read(file_fixture("certificates.json")))
-      @project_ids = ["firebase-id-token"]
+      setup_jwt_authentication
     end
 
     def verify_credentials
@@ -22,11 +19,6 @@ RSpec.describe "Sessions", type: :request do
       @headers ||= {}
       @headers["Content-Type"] = "application/json"
       @headers
-    end
-
-    def stub_firebase_id_token
-      allow_any_instance_of(FirebaseIdToken::Certificates).to receive(:local_certs).and_return(@certificate)
-      allow_any_instance_of(FirebaseIdToken::Configuration).to receive(:project_ids).and_return(@project_ids)
     end
 
     def stub_twitter_reuest
