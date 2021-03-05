9b951039e7b8310ff8ad5362d47e117cb559d631
mh-mobile <mh.mobiler@gmail.com>
2021-03-05 19:12:48 +0900

    tweet_crawler_spec.rbのsub_clientの処理を共通化

--- api/spec/models/tweet_crawler_spec.rb
+++ api/spec/models/tweet_crawler_spec.rb
@@ -3,6 +3,40 @@
 require "rails_helper"
 
 RSpec.describe TweetCrawler, type: :model do
+  let!(:stub_client) do
+    stub_connection = Faraday.new do |connection|
+      connection.use FaradayMiddleware::FollowRedirects
+      connection.request :url_encoded
+      connection.response :mashify, mash_class: EventResponse
+      connection.response :json
+      connection.response :raise_error
+      connection.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
+        stub.get("search/tweets.json") do |env|
+          q = env.params["q"]
+          if q == "1"
+            [200, {}, read_fixture_json("twitter_search_100_count_results.json")]
+          elsif q == "2"
+            [200, {}, read_fixture_json("twitter_search_5_count_results.json")]
+          elsif q == "3"
+            [200, {}, read_fixture_json("twitter_search_100_count_results.json")]
+          elsif q == "4"
+            [200, {}, read_fixture_json("twitter_search_5_count_results.json")]
+          end
+        end
+      end
+    end
+
+    twitter_client = TwitterClient.new(
+      app_token: "test_app_token",
+      oauth_consumer_key: "test_oauth_consumer_key",
+      oauth_token: "test_oauth_token",
+      oauth_consumer_secret: "test_oauth_consumer_secret",
+      oauth_token_secret: "test_oauth_token_secret"
+    )
+    allow(twitter_client).to receive(:connection).and_return(stub_connection)
+    twitter_client
+  end
+
   describe "ツイートのクロール設定の更新" do
     let!(:tweet_crawl_setting_1) do
       FactoryBot.create(:tweet_crawl_setting_1)
@@ -24,33 +58,8 @@ RSpec.describe TweetCrawler, type: :model do
       FactoryBot.build(:hoge)
     end
 
-    let!(:stub_client) do
-      stub_connection = Faraday.new do |connection|
-        connection.use FaradayMiddleware::FollowRedirects
-        connection.request :url_encoded
-        connection.response :mashify, mash_class: EventResponse
-        connection.response :json
-        connection.response :raise_error
-        connection.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
-          stub.get("search/tweets.json") do |env|
-            [200, {}, read_fixture_json("twitter_search_100_count_results.json")]
-          end
-        end
-      end
-
-      twitter_client = TwitterClient.new(
-        app_token: "test_app_token",
-        oauth_consumer_key: "test_oauth_consumer_key",
-        oauth_token: "test_oauth_token",
-        oauth_consumer_secret: "test_oauth_consumer_secret",
-        oauth_token_secret: "test_oauth_token_secret"
-      )
-      allow(twitter_client).to receive(:connection).and_return(stub_connection)
-      twitter_client
-    end
-
     let(:search_tweets) do
-      body = stub_client.search(q: "(url:doorkeeper.jp/events OR url:connpass.com/event/) -filter:retweets filter:links", max_id: "0", since_id: "0")
+      body = stub_client.search(q: "1", max_id: "0", since_id: "0")
       body.statuses
     end
 
@@ -79,33 +88,8 @@ RSpec.describe TweetCrawler, type: :model do
       FactoryBot.build(:hoge)
     end
 
-    let!(:stub_client) do
-      stub_connection = Faraday.new do |connection|
-        connection.use FaradayMiddleware::FollowRedirects
-        connection.request :url_encoded
-        connection.response :mashify, mash_class: EventResponse
-        connection.response :json
-        connection.response :raise_error
-        connection.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
-          stub.get("search/tweets.json") do |env|
-            [200, {}, read_fixture_json("twitter_search_5_count_results.json")]
-          end
-        end
-      end
-
-      twitter_client = TwitterClient.new(
-        app_token: "test_app_token",
-        oauth_consumer_key: "test_oauth_consumer_key",
-        oauth_token: "test_oauth_token",
-        oauth_consumer_secret: "test_oauth_consumer_secret",
-        oauth_token_secret: "test_oauth_token_secret"
-      )
-      allow(twitter_client).to receive(:connection).and_return(stub_connection)
-      twitter_client
-    end
-
     let(:search_tweets) do
-      body = stub_client.search(q: "(url:doorkeeper.jp/events OR url:connpass.com/event/) -filter:retweets filter:links", max_id: "0", since_id: "0")
+      body = stub_client.search(q: "2", max_id: "0", since_id: "0")
       body.statuses
     end
 
@@ -134,33 +118,8 @@ RSpec.describe TweetCrawler, type: :model do
       FactoryBot.build(:hoge)
     end
 
-    let!(:stub_client) do
-      stub_connection = Faraday.new do |connection|
-        connection.use FaradayMiddleware::FollowRedirects
-        connection.request :url_encoded
-        connection.response :mashify, mash_class: EventResponse
-        connection.response :json
-        connection.response :raise_error
-        connection.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
-          stub.get("search/tweets.json") do |env|
-            [200, {}, read_fixture_json("twitter_search_100_count_results.json")]
-          end
-        end
-      end
-
-      twitter_client = TwitterClient.new(
-        app_token: "test_app_token",
-        oauth_consumer_key: "test_oauth_consumer_key",
-        oauth_token: "test_oauth_token",
-        oauth_consumer_secret: "test_oauth_consumer_secret",
-        oauth_token_secret: "test_oauth_token_secret"
-      )
-      allow(twitter_client).to receive(:connection).and_return(stub_connection)
-      twitter_client
-    end
-
     let(:search_tweets) do
-      body = stub_client.search(q: "(url:doorkeeper.jp/events OR url:connpass.com/event/) -filter:retweets filter:links", max_id: "105", since_id: "1")
+      body = stub_client.search(q: "3", max_id: "105", since_id: "1")
       body.statuses
     end
 
@@ -189,33 +148,8 @@ RSpec.describe TweetCrawler, type: :model do
       FactoryBot.create(:user_id_100)
     end
 
-    let!(:stub_client) do
-      stub_connection = Faraday.new do |connection|
-        connection.use FaradayMiddleware::FollowRedirects
-        connection.request :url_encoded
-        connection.response :mashify, mash_class: EventResponse
-        connection.response :json
-        connection.response :raise_error
-        connection.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
-          stub.get("search/tweets.json") do |env|
-            [200, {}, read_fixture_json("twitter_search_5_count_results.json")]
-          end
-        end
-      end
-
-      twitter_client = TwitterClient.new(
-        app_token: "test_app_token",
-        oauth_consumer_key: "test_oauth_consumer_key",
-        oauth_token: "test_oauth_token",
-        oauth_consumer_secret: "test_oauth_consumer_secret",
-        oauth_token_secret: "test_oauth_token_secret"
-      )
-      allow(twitter_client).to receive(:connection).and_return(stub_connection)
-      twitter_client
-    end
-
     let(:search_tweets) do
-      body = stub_client.search(q: "(url:doorkeeper.jp/events OR url:connpass.com/event/) -filter:retweets filter:links", max_id: "105", since_id: "1")
+      body = stub_client.search(q: "4", max_id: "105", since_id: "1")
       body.statuses
     end
 
