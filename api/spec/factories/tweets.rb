b23ed172f294618a4edf5d3853f0cba4d9d484c7
mh-mobile <mh.mobiler@gmail.com>
2021-03-05 15:18:47 +0900

    RetweetCrawlerのテストケースを追加

--- api/spec/factories/tweets.rb
+++ api/spec/factories/tweets.rb
@@ -5,5 +5,37 @@ FactoryBot.define do
     sequence(:id) { |n| "#{n}" }
     sequence(:text) { |n| "ツイート内容: #{n}" }
     tweeted_at { Time.current }
+
+    trait :tweet_three_hours_ago do
+      text { |n| "tweet at tweet_treee_hours_ago" }
+      tweeted_at { Time.current.ago(3.hours) }
+    end
+
+    trait :tweet_four_hours_ago do
+      text { |n| "tweet at tweet_four_hours_ago" }
+      tweeted_at { Time.current.ago(4.hours) }
+    end
+
+    trait :tweet_twenty_three_hours_ago do
+      text { |n| "tweet at tweet_twenty_three_hours_ago" }
+      tweeted_at { Time.current.ago(23.hour) }
+    end
+
+    trait :tweet_twenty_five_hours_ago do
+      text { |n| "tweet at tweet_twenty_five_hours_ago" }
+      tweeted_at { Time.current.ago(25.hours) }
+    end
+
+    trait :tweet_last_udpate_two_days_ago do
+      text { |n| "update at two_days_ago" }
+      tweeted_at { Time.current.ago(10.hours) }
+      retweet_last_updated_at { Time.current.ago(2.days) }
+    end
+
+    trait :tweet_last_udpate_three_days_ago do
+      text { |n| "update at three_days_ago" }
+      tweeted_at { Time.current.ago(10.hours) }
+      retweet_last_updated_at { Time.current.ago(3.days) }
+    end
   end
 end
