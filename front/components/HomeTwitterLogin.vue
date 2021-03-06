87f3072fe61a3ece1b135740776025ffe1314dea
mh-mobile <mh.mobiler@gmail.com>
2021-03-06 18:39:58 +0900

    Twitterログインできない場合のエラー表示

--- front/components/HomeTwitterLogin.vue
+++ front/components/HomeTwitterLogin.vue
@@ -1,32 +1,64 @@
 <template lang="pug">
-.home_twitter_login
-  .twitter_login_button(@click="loginByTwitter")
-    | Twitterで始める
-  .twitter_agreement
-    .agreement_upper
-      | アカウントを作成することで
-      a(href="/terms")
-        | 利用規約
-      |・
-    .agreement_lower
-      a(href="/privacy_policy")
-        | プライバシーポリシー
-      | に同意したものとみなします。
+div
+  .home_twitter_login
+    .twitter_login_button(@click="loginByTwitter")
+      | Twitterで始める
+    .twitter_agreement
+      .agreement_upper
+        | アカウントを作成することで
+        a(href="/terms")
+          | 利用規約
+        |・
+      .agreement_lower
+        a(href="/privacy_policy")
+          | プライバシーポリシー
+        | に同意したものとみなします。
+  Modal(@close="closeModal" v-if="modal")
+    .modal-container
+      .title-container
+        .title
+          | ログインできませんでした。
+      .button-container
+        .ok-button(@click="closeModal")
+          | OK 
 
 </template>
 
 <script lang="ts">
-import { defineComponent, useContext } from "@nuxtjs/composition-api"
+import {
+  defineComponent,
+  useContext,
+  reactive,
+  toRefs
+} from "@nuxtjs/composition-api"
+import Modal from "@/components/Modal.vue"
+import { useModalHelper } from "@/compositions/modal_helper"
 import firebase from "firebase/app"
 import "firebase/auth"
 const Cookie = process.client ? require("js-cookie") : undefined
 
 export default defineComponent({
+  components: { Modal },
   setup(props, { root }) {
+    const state = reactive({
+      modal: false
+    })
     if (process.server) {
-      return
+      return {
+        ...toRefs(state)
+      }
     }
     const { store } = useContext()
+
+    const { setScrollEnabled } = useModalHelper()
+    const openModal = () => {
+      state.modal = true
+      setScrollEnabled(false)
+    }
+    const closeModal = () => {
+      state.modal = false
+      setScrollEnabled(true)
+    }
 
     const loginByTwitter = () => {
       const provider = new firebase.auth.TwitterAuthProvider()
@@ -63,11 +95,15 @@ export default defineComponent({
         })
         .catch(function (error) {
           root.$sentry.captureException(error)
+          openModal()
         })
     }
 
     return {
-      loginByTwitter
+      ...toRefs(state),
+      loginByTwitter,
+      openModal,
+      closeModal
     }
   }
 })
@@ -121,6 +157,50 @@ export default defineComponent({
     .agreement_lower {
       margin-top: 2px;
       font-size: 0.8em;
+    }
+  }
+}
+
+.modal-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-width: 30em;
+  padding: 20px;
+  .title-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .title {
+      color: #000000;
+      font-size: 1em;
+      font-weight: normal;
+    }
+  }
+  .button-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 40px;
+
+    .ok-button {
+      width: 200px;
+      height: 44px;
+      background: #218af5;
+      color: #fff;
+      font-size: 1em;
+      font-weight: bold;
+      padding: 15px 40px;
+      border-radius: 10px;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-right: 20px;
+      &:hover {
+        opacity: 0.8;
+      }
     }
   }
 }
