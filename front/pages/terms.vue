90e0b2c32b64fff916627c408dc856e3943dc27d
mh-mobile <mh.mobiler@gmail.com>
2021-04-04 17:53:51 +0900

    feat: プライバシーポリシーと利用規約をネイティブアプリ対応

--- front/pages/terms.vue
+++ front/pages/terms.vue
@@ -10,21 +10,33 @@ export default defineComponent({
   components: {
     TermsContent
   },
-  layout: "unauthorized_default"
+  layout: (context) => context.$device.isMobile ? "" : "unauthorized_default",
 })
 </script>
 
 <style lang="scss" scoped>
-.terms_container {
-  max-width: 50rem;
-  min-width: 50rem;
-  margin: 20px auto;
-  padding: 20px 30px;
-  background-color: #ffffff;
-  border-radius: 5px;
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-  min-height: calc(100vh - 200px);
+@media all and (min-width: 480px) {
+  .terms_container {
+    max-width: 50rem;
+    min-width: 50rem;
+    margin: 20px auto;
+    padding: 20px 30px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    min-height: calc(100vh - 200px);
+  }
+}
+
+@media all and (max-width: 480px) {
+  .terms_container {
+    padding: 20px 30px;
+    background-color: #ffffff;
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+  }
 }
 </style>
