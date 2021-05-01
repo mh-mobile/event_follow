ddea03792d2c1b699aa9003bbdf39ae443f83a60
mh-mobile <mh.mobiler@gmail.com>
2021-05-01 22:15:09 +0900

    fix: nuxt.config.jsのlinkタグの重複エラーを解消

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -7,8 +7,10 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    link: [{ rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/yakuhanjp@3.3.1/dist/css/yakuhanjp.min.css" }]
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/yakuhanjp@3.3.1/dist/css/yakuhanjp.min.css" }
+    ]
   },
   loading: {
     color: "#0099e5",
