ae370bf4c2a261eb1331b951a70017908d2b5b13
mh-mobile <mh.mobiler@gmail.com>
2021-05-01 22:00:43 +0900

    fix: タイトルの最初が括弧の場合の見栄えをよくする

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -7,7 +7,8 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/yakuhanjp@3.3.1/dist/css/yakuhanjp.min.css" }]
   },
   loading: {
     color: "#0099e5",
