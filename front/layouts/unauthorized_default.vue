abbe097c5788387d13280533c4537ec4cd9a2f2c
mh-mobile <mh.mobiler@gmail.com>
2021-05-06 12:30:16 +0900

    fix: PC版のホーム画面のレイアウトの改善

--- front/layouts/unauthorized_default.vue
+++ front/layouts/unauthorized_default.vue
@@ -8,8 +8,9 @@ div
       .github
         a(href="https://github.com/mh-mobile/event_follow" target="_blank")
           img(src="@/assets/github_logo.png")
-      a(href="/terms" class="terms") 利用規約
-      a(href="/privacy_policy" class="privacy_policy") プライバシーポリシー
+      .agreement_container
+        a(href="/terms" class="terms") 利用規約
+        a(href="/privacy_policy" class="privacy_policy") プライバシーポリシー
 </template>
 
 <style lang="scss">
@@ -32,16 +33,16 @@ html {
   .footer {
     padding: 5px;
     background-color: #efe9e5;
+    height: 160px;
 
     .footer-container {
       max-width: 50rem;
-      margin: 0 auto;
+      margin: 20px auto;
       padding: 5px;
-      height: 70px;
       display: flex;
-      flex-direction: row;
+      flex-direction: column;
       align-items: center;
-      justify-content: flex-end;
+      justify-content: center;
       box-sizing: border-box;
       -moz-box-sizing: border-box;
       -webkit-box-sizing: border-box;
@@ -53,18 +54,34 @@ html {
       }
 
       .github {
-        margin-right: auto;
         width: 20px;
-        height: 20px;
+        height: auto;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        &:hover {
+          opacity: 0.8;
+        }
       }
 
-      .terms {
-        margin-right: 50px;
-        color: #000;
-      }
+      .agreement_container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        .terms {
+          color: #000;
+          padding-right: 10px;
+          &:hover {
+            opacity: 0.8;
+          }
+        }
 
-      .privacy_policy {
-        color: #000;
+        .privacy_policy {
+          color: #000;
+          &:hover {
+            opacity: 0.8;
+          }
+        }
       }
     }
   }
