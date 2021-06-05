2375886e67700f16bbb8a1eee76fabfb4bee9092
mh-mobile <mh.mobiler@gmail.com>
2021-06-05 14:27:33 +0900

    refactor: Api::FriendshipsController#find_user_idsの改善

--- api/app/controllers/api/friendships_controller.rb
+++ api/app/controllers/api/friendships_controller.rb
@@ -8,10 +8,10 @@ class Api::FriendshipsController < Api::BaseController
   private
     def find_user_ids
       user_ids = params[:user_ids]
-      if user_ids.present? && user_ids.split(",").count > 0
-        user_ids.split(",")
-      else
-        @user_ids = []
-      end
+      exists?(user_ids) ? user_ids.split(",") : []
+    end
+
+    def exists?(user_ids)
+      user_ids.present? && user_ids.split(",").count > 0
     end
 end
