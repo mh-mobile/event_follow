b95f3b53bbe39f4aeab9cb3b37c902048fe5174e
mh-mobile <mh.mobiler@gmail.com>
2021-06-05 16:35:43 +0900

    delete: 不要な末尾のカンマを削除

--- front/middleware/response_header.ts
+++ front/middleware/response_header.ts
@@ -4,5 +4,5 @@ export default (_: any, res: any, next: any) => {
   res.setHeader("X-Content-Type-Options", "nosniff")
   res.setHeader("X-Frame-Options", "SAMEORIGIN")
   res.setHeader("X-Download-Options", "noopen")
-  next();
-};
+  next()
+}
