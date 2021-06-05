9ff820ee66a9c2190546913506fca14a05fc1638
mh-mobile <mh.mobiler@gmail.com>
2021-06-05 14:39:29 +0900

    refactor: Api::UsersController#delete_user_sessionの処理についてコメントで補足

--- api/app/controllers/api/users_controller.rb
+++ api/app/controllers/api/users_controller.rb
@@ -7,6 +7,8 @@ class Api::UsersController < Api::BaseController
   end
 
   private
+    # 他のTwitterユーザーからフォローされている可能性があるため、
+    # カレントユーザー自体はUsersテーブルから削除しない
     def delete_user_session
       current_user.user_event_setting = nil
       current_user.user_token = nil
